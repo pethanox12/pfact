b697094242b354a0a01393a98f8e8cc1e721e17e
Adrian Talbot <adrian.r.talbot@gmail.com>
2024-01-01 15:47:30 +0000

    Initial Commit

---
+++ pfact/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  pfact
+//
+//  Created by Adrian Talbot on 01/01/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
